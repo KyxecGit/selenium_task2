6c206e93b34f845d41d3b8b35b17c76b097f4015
Kyxec <kyxec.working.com>
2024-09-24 03:00:29 +0600

    update

--- pages/result_page.py
+++ pages/result_page.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from .base_page import BasePage
 
@@ -7,7 +8,7 @@ class ResultPage(BasePage):
     GAME_LIST = (By.CSS_SELECTOR, ".search_result_row")
     DROPDOWN = (By.ID, "sort_by_trigger")
     SORT_BY_PRICE = (By.ID, "Price_DESC")
-    LOADING_INDICATOR = (By.ID, 'search_results_loading')
+    SEARCH_RESULT_CONTAINER = (By.ID, "search_result_container")
 
     def set_filter_price(self):
         dropdown = self.wait.until(EC.element_to_be_clickable(self.DROPDOWN))
@@ -20,4 +21,8 @@ class ResultPage(BasePage):
         return len(games)
 
     def wait_for_filter_to_apply(self):
-        self.wait.until(EC.invisibility_of_element_located(self.LOADING_INDICATOR))
+        short_wait = WebDriverWait(self.driver, 10, poll_frequency=0.05)
+        search_result_container = short_wait.until(EC.presence_of_element_located(self.SEARCH_RESULT_CONTAINER))
+        short_wait.until(lambda driver: search_result_container.get_attribute("style") != "")
+
+
